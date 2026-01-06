81904f86408c6d12b52307e6f58203ef6ccb01df
Giovanni Junseo Kim <kingjunseo@gmail.com>
2026-01-06 21:39:47 +0900

    feat: 폰트 시스템 추가

--- app/src/main/java/com/flint/core/designsystem/theme/Theme.kt
+++ app/src/main/java/com/flint/core/designsystem/theme/Theme.kt
@@ -2,22 +2,33 @@ package com.flint.core.designsystem.theme
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.staticCompositionLocalOf
-
-private val LocalFlintColors =
-    staticCompositionLocalOf<FlintColors> { error("No FlintColors provided") }
-
-object FlintTheme {
-    val colors: FlintColors
-        @Composable
-        get() = LocalFlintColors.current
-}
 
 @Composable
 fun FlintTheme(content: @Composable () -> Unit) {
     CompositionLocalProvider(
         LocalFlintColors provides Colors,
+        LocalFlintTypography provides Typography,
     ) {
         content()
     }
 }
+
+object FlintTheme {
+    val colors: FlintColors
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalFlintColors.current
+
+    val typography: FlintTypography
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalFlintTypography.current
+}
+
+private val LocalFlintColors =
+    staticCompositionLocalOf<FlintColors> { error("No FlintColors provided") }
+
+private val LocalFlintTypography =
+    staticCompositionLocalOf<FlintTypography> { error("No FlintTypography provided") }
