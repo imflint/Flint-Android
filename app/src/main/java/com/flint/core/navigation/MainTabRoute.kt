e70fd876a9aed8ce9f30cb8513fb4de7e9054b34
Nahyun Kim <hedghog1004@naver.com>
2026-01-12 17:45:07 +0900

    feat: Route 인터페이스 추가

---
+++ app/src/main/java/com/flint/core/navigation/MainTabRoute.kt
@@ -0,0 +1,14 @@
+package com.flint.core.navigation
+
+import kotlinx.serialization.Serializable
+
+interface MainTabRoute: Route {
+    @Serializable
+    data object Home : MainTabRoute
+
+    @Serializable
+    data object Explore : MainTabRoute
+
+    @Serializable
+    data object Profile : MainTabRoute
+}
