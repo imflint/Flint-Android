95c2eb613f7b7291bd65e6d84e71a04fe275d806
Nahyun Kim <hedghog1004@naver.com>
2026-01-12 17:51:26 +0900

    feat: MainTab 정의

---
+++ app/src/main/java/com/flint/presentation/main/MainTab.kt
@@ -0,0 +1,38 @@
+package com.flint.presentation.main
+
+import androidx.annotation.DrawableRes
+import com.flint.R
+import com.flint.core.navigation.MainTabRoute
+import com.flint.core.navigation.Route
+
+enum class MainTab(
+    @DrawableRes val iconResId: Int,
+    val route: MainTabRoute,
+    val label: String
+) {
+    HOME(
+        iconResId = R.drawable.ic_home_empty,
+        route = MainTabRoute.Home,
+        label = "홈"
+    ),
+    EXPLORE(
+        iconResId = R.drawable.ic_explore_empty,
+        route = MainTabRoute.Explore,
+        label = "탐색"
+    ),
+    PROFILE(
+        iconResId = R.drawable.ic_my_empty,
+        route = MainTabRoute.Profile,
+        label = "MY"
+    );
+
+    companion object {
+        fun find(predicate: (MainTabRoute) -> Boolean): MainTab? {
+            return MainTab.entries.find { predicate(it.route) }
+        }
+
+        fun contains(predicate: (Route) -> Boolean): Boolean {
+            return MainTab.entries.map { it.route }.any { predicate(it) }
+        }
+    }
+}
