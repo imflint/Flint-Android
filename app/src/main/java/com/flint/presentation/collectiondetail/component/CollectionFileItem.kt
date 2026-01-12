46d7040d2fa85f7dbaee4e10f29c7923c677fc26
Nahyun Kim <hedghog1004@naver.com>
2026-01-13 01:44:58 +0900

    feat: 컬렉션 파일 기본 아이템 구현

---
+++ app/src/main/java/com/flint/presentation/collectiondetail/component/CollectionFileItem.kt
@@ -0,0 +1,152 @@
+package com.flint.presentation.collectiondetail.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.flint.R
+import com.flint.core.common.extension.dropShadow
+import com.flint.core.designsystem.theme.FlintTheme
+
+@Composable
+fun CollectionFileItem() {
+    Box(
+        modifier = Modifier
+            .size(width = 154.dp, height = 154.dp)
+            .clip(RoundedCornerShape(12.dp))
+            .background(FlintTheme.colors.gray800)
+    ) {
+
+        OverlappingCards(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(end = 16.dp, bottom = 4.dp)
+        )
+
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(108.dp)
+                .align(Alignment.BottomEnd)
+                .paint(
+                    painter = painterResource(id = R.drawable.img_folder_fg),
+                    contentScale = ContentScale.FillWidth
+                )
+                .padding(12.dp)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.End,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp)
+                    .align(Alignment.TopEnd)
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Image(
+                        imageVector = ImageVector.vectorResource(R.drawable.ic_bookmark_fill),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(24.dp)
+                    )
+                    Text(
+                        "24",
+                        style = TextStyle(
+                            color = Color.White
+                        ),
+                        modifier = Modifier
+                    )
+                }
+            }
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(12.dp),
+                modifier = Modifier
+                    .align(Alignment.BottomStart)
+                    .fillMaxWidth()
+            ){
+                Image(
+                    imageVector = ImageVector.vectorResource(R.drawable.ic_avatar_blue),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(28.dp)
+                )
+                Text(
+                    "닉네임",
+                    style = FlintTheme.typography.caption1M12,
+                    color = Color.White
+                )
+            }
+
+        }
+    }
+}
+
+@Composable
+private fun OverlappingCards(
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+    ) {
+        // 앞쪽 카드 (똑바로 있는 카드)
+        Box(
+            modifier = Modifier
+                .size(width = 80.dp, height = 120.dp)
+                .clip(RoundedCornerShape(12.dp))
+                .background(Color.White)
+        )
+        // 뒤쪽 카드 (회전되고 오프셋된 카드)
+        Box(
+            modifier = Modifier
+                .dropShadow(
+                    shape = CircleShape,
+                    color = Color(0xFF000000).copy(alpha = 0.35f),
+                    offsetX = (-4).dp,
+                    offsetY = 0.dp,
+                    blur = 10.dp,
+                    spread = 0.dp
+                )
+                .size(width = 80.dp, height = 120.dp)
+                .rotate(15f)
+                .offset(x = 24.dp, y = 5.dp)
+                .clip(RoundedCornerShape(12.dp))
+                .background(Color.White)
+        )
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+private fun Preview() {
+    FlintTheme {
+        CollectionFileItem()
+    }
+}
