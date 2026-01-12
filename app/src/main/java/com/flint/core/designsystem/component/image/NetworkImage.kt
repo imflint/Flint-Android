ba952685dd0dd93527de1729d6448aa8ce2449e0
Nahyun Kim <hedghog1004@naver.com>
2026-01-13 02:07:14 +0900

    feat: 프리뷰용 NetworkImage 설정

--- app/src/main/java/com/flint/core/designsystem/component/image/NetworkImage.kt
+++ app/src/main/java/com/flint/core/designsystem/component/image/NetworkImage.kt
@@ -1,5 +1,9 @@
 package com.flint.core.designsystem.component.image
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -7,7 +11,12 @@ import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
+import com.flint.R
 import com.flint.core.designsystem.theme.FlintTheme
 
 @Composable
@@ -18,11 +27,39 @@ fun NetworkImage(
     contentScale: ContentScale = ContentScale.Crop,
     contentDescription: String? = null
 ) {
-    AsyncImage(
-        model = imageUrl,
-        contentDescription = contentDescription,
-        contentScale = contentScale,
-        error = ColorPainter(FlintTheme.colors.gray200),
-        modifier = modifier.clip(shape)
-    )
+    if (LocalInspectionMode.current) { // 프리뷰 확인용
+        Image(
+            painter = painterResource(R.drawable.img_dummy_poster),
+            contentDescription = contentDescription,
+            contentScale = contentScale,
+            modifier = modifier.clip(shape)
+
+        )
+    } else {
+        AsyncImage(
+            model = imageUrl,
+            contentDescription = contentDescription,
+            contentScale = contentScale,
+            error = ColorPainter(FlintTheme.colors.gray200),
+            modifier = modifier.clip(shape)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun NetworkImagePreview() {
+    FlintTheme {
+        Row {
+            NetworkImage(
+                imageUrl = "",
+                modifier = Modifier.size(50.dp)
+            )
+            NetworkImage(
+                imageUrl = "",
+                shape = CircleShape,
+                modifier = Modifier.size(50.dp)
+            )
+        }
+    }
 }
