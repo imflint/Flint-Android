c50b2b53829bab5eb2c8e5ea229beafb1f4d2796
ckals413 <ckals413@naver.com>
2026-01-13 22:45:33 +0900

    style: ktlint적용

--- app/src/main/java/com/flint/core/designsystem/component/image/SelectedFilmItem.kt
+++ app/src/main/java/com/flint/core/designsystem/component/image/SelectedFilmItem.kt
@@ -3,7 +3,6 @@ package com.flint.core.designsystem.component.image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
