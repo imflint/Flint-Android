5209992f5cd7a352d7fb388fe9578363e71a7c66
ckals413 <ckals413@naver.com>
2026-01-13 20:13:47 +0900

    style: ktlint적용

--- app/src/main/java/com/flint/presentation/onboarding/component/OnboardingOttItem.kt
+++ app/src/main/java/com/flint/presentation/onboarding/component/OnboardingOttItem.kt
@@ -19,7 +19,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
