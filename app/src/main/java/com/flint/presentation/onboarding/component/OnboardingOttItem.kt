ed9df103077ef448c0446e002ae46bcbba1a42be
ckals413 <ckals413@naver.com>
2026-01-13 20:13:00 +0900

    style: 이미지벡터로 변경

--- app/src/main/java/com/flint/presentation/onboarding/component/OnboardingOttItem.kt
+++ app/src/main/java/com/flint/presentation/onboarding/component/OnboardingOttItem.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -16,8 +17,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -62,7 +65,7 @@ fun OnboardingOttItem(
                         .background(FlintTheme.colors.overlay)
                 )
                 Icon(
-                    painter = painterResource(id = R.drawable.ic_onboarding_film_check),
+                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_onboarding_film_check),
                     contentDescription = "선택됨",
                     tint = FlintTheme.colors.white,
                     modifier =
@@ -81,7 +84,7 @@ fun OnboardingOttItem(
             style = FlintTheme.typography.body1M16,
             color = if (isSelected) FlintTheme.colors.gray300 else FlintTheme.colors.white,
             textAlign = TextAlign.Center,
-            modifier = Modifier.width(100.dp)
+            modifier = Modifier.fillMaxWidth()
         )
     }
 }
