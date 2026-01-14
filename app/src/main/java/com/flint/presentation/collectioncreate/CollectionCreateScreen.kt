dd93f5f75f0d952b01a93680be92e4ddb663faf6
너의이름 <qkrcksal1052@gmail.com>
2026-01-15 01:35:10 +0900

    refactor: CollectionCreateScreen LazyColumn으로 마이그레이션

--- app/src/main/java/com/flint/presentation/collectioncreate/CollectionCreateScreen.kt
+++ app/src/main/java/com/flint/presentation/collectioncreate/CollectionCreateScreen.kt
@@ -3,6 +3,7 @@ package com.flint.presentation.collectioncreate
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
@@ -11,11 +12,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -34,18 +35,26 @@ import com.flint.presentation.collectioncreate.component.CollectionAddFilmBottom
 import com.flint.presentation.collectioncreate.component.CollectionCreateFilmItemList
 import com.flint.presentation.collectioncreate.component.CollectionCreateThumbnail
 
-// @Composable
-// fun CollectionCreateRoute(
-//    paddingValues: PaddingValues,
-//    navigateToAddFilm: () -> Unit,
-// ) {
-//    CollectionCreateScreen(
-//        thumbnailImageUrl = "",
-//        onBackClick = {},
-//        onGalleryClick = {},
-//        onCoverDeleteClick = {},
-//    )
-// }
+@Composable
+fun CollectionCreateRoute(
+    paddingValues: PaddingValues,
+    navigateToAddFilm: () -> Unit,
+) {
+    CollectionCreateScreen(
+        thumbnailImageUrl = "",
+        onBackClick = {},
+        onGalleryClick = {},
+        onCoverDeleteClick = {},
+    )
+}
+
+data class CollectionFilmUiModel(
+    val filmId: Long,
+    val imageUrl: String,
+    val title: String,
+    val director: String,
+    val createdYear: String,
+)
 
 @Composable
 fun CollectionCreateScreen(
@@ -56,10 +65,27 @@ fun CollectionCreateScreen(
 ) {
     var titleText by remember { mutableStateOf("") }
     var contentText by remember { mutableStateOf("") }
-
     var isSheetVisible by remember { mutableStateOf(false) }
 
-    val scrollState = rememberScrollState()
+    val filmList =
+        remember {
+            mutableStateListOf(
+                CollectionFilmUiModel(
+                    filmId = 1L,
+                    imageUrl = "https://buly.kr/DEaVFRZ",
+                    title = "해리포터 불의 잔",
+                    director = "마이크 뉴웰",
+                    createdYear = "2005",
+                ),
+                CollectionFilmUiModel(
+                    filmId = 2L,
+                    imageUrl = "https://buly.kr/DEaVFRZ",
+                    title = "인터스텔라",
+                    director = "크리스토퍼 놀란",
+                    createdYear = "2014",
+                ),
+            )
+        }
 
     Column(
         modifier =
@@ -67,37 +93,30 @@ fun CollectionCreateScreen(
                 .fillMaxWidth()
                 .background(color = FlintTheme.colors.background),
     ) {
-        // TopAppBar
         FlintBackTopAppbar(onClick = onBackClick)
 
-        Column(
-            modifier =
-                Modifier
-                    .weight(1f)
-                    .verticalScroll(scrollState),
+        LazyColumn(
+            modifier = Modifier.weight(1f),
+            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 36.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
         ) {
             // 썸네일
-            CollectionCreateThumbnail(
-                imageUrl = thumbnailImageUrl,
-                onClick = { isSheetVisible = true },
-            )
+            item {
+                CollectionCreateThumbnail(
+                    imageUrl = thumbnailImageUrl,
+                    onClick = { isSheetVisible = true },
+                )
+            }
 
-            Spacer(Modifier.height(36.dp))
-
-            Column(
-                modifier = Modifier.padding(horizontal = 16.dp),
-                verticalArrangement = Arrangement.spacedBy(28.dp),
-            ) {
-                // 컬렉션 제목 필드란
+            // 컬렉션 제목
+            item {
                 Column {
                     Text(
                         text = "컬렉션 제목",
                         color = FlintTheme.colors.white,
                         style = FlintTheme.typography.head3M18,
                     )
-
                     Spacer(Modifier.height(16.dp))
-
                     FlintLongTextField(
                         modifier = Modifier.fillMaxWidth(),
                         value = titleText,
@@ -107,17 +126,17 @@ fun CollectionCreateScreen(
                         height = 40.dp,
                     )
                 }
+            }
 
-                // 컬렉션 소개 필드란
+            // 컬렉션 소개
+            item {
                 Column {
                     Text(
                         text = "컬렉션 소개 (선택)",
                         color = FlintTheme.colors.white,
                         style = FlintTheme.typography.head3M18,
                     )
-
                     Spacer(Modifier.height(16.dp))
-
                     FlintLongTextField(
                         modifier = Modifier.fillMaxWidth(),
                         value = contentText,
@@ -127,15 +146,16 @@ fun CollectionCreateScreen(
                         height = 104.dp,
                     )
                 }
+            }
 
-                // 컬렉션 공개 여부
+            // 컬렉션 공개 여부
+            item {
                 Column {
                     Text(
                         text = "컬렉션 공개 여부",
                         color = FlintTheme.colors.white,
                         style = FlintTheme.typography.head3M18,
                     )
-
                     Spacer(Modifier.height(16.dp))
 
                     Row {
@@ -146,9 +166,7 @@ fun CollectionCreateScreen(
                             onClick = {},
                             modifier = Modifier.weight(1f),
                         )
-
                         Spacer(Modifier.width(8.dp))
-
                         FlintIconButton(
                             text = "비공개",
                             iconRes = R.drawable.ic_lock,
@@ -158,8 +176,10 @@ fun CollectionCreateScreen(
                         )
                     }
                 }
+            }
 
-                // 컬렉션 작품 추가
+            // 작품 추가 헤더
+            item {
                 Column {
                     Text(
                         text = "작품 추가",
@@ -178,26 +198,35 @@ fun CollectionCreateScreen(
                             style = FlintTheme.typography.body2R14,
                         )
                         Text(
-                            text = "0/10",
+                            text = "${filmList.size}/10",
                             color = FlintTheme.colors.gray200,
                             style = FlintTheme.typography.body2R14,
                         )
                     }
+                }
+            }
 
-                    Spacer(Modifier.height(16.dp))
+            // 작품 리스트
+            items(
+                items = filmList,
+                key = { it.filmId },
+            ) { film ->
+                Column {
+                    CollectionCreateFilmItemList(
+                        onCancelClick = {
+                            filmList.removeAll { it.filmId == film.filmId }
+                        },
+                        imageUrl = film.imageUrl,
+                        title = film.title,
+                        director = film.director,
+                        createdYear = film.createdYear,
+                    )
+                }
+            }
 
-                    LazyColumn {
-                        item {
-                            CollectionCreateFilmItemList(
-                                onCancelClick = {},
-                                imageUrl = "https://buly.kr/DEaVFRZ",
-                                title = "해리포터 불의 잔 해리포터 불의 잔 해리포터 불의 잔 해리포터 불의 잔 해리포터 불의 잔 해리포터 불의 잔",
-                                director = "메롱",
-                                createdYear = "2005",
-                            )
-                        }
-                    }
-
+            // 작품 추가 버튼
+            item {
+                Column {
                     FlintIconButton(
                         text = "작품 추가하기",
                         iconRes = R.drawable.ic_plus,
@@ -210,9 +239,7 @@ fun CollectionCreateScreen(
                     )
                 }
             }
-            Spacer(Modifier.height(36.dp))
         }
-
         FlintLargeButton(
             text = "완료",
             state = FlintButtonState.Disable,
