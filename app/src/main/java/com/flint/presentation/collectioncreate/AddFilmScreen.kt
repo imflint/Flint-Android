b6b02b270f737f9a1dfc28344c63d495137e7d50
너의이름 <qkrcksal1052@gmail.com>
2026-01-15 04:06:52 +0900

    feat: 작품 추가하기 화면 구현
    - `AddFilmScreen` 컴포저블을 새로 추가했습니다.
    - `FlintBackTopAppbar`와 `FlintSearchTextField`를 사용하여 화면의 기본 UI를 구성했습니다.
    - `LazyColumn`과 `CollectionCreateFilmSelect` 컴포저블을 이용해 작품 목록과 선택 기능을 구현했습니다.
    - `CollectionFilmUiModel` 데이터 클래스를 정의했습니다.

--- app/src/main/java/com/flint/presentation/collectioncreate/AddFilmScreen.kt
+++ app/src/main/java/com/flint/presentation/collectioncreate/AddFilmScreen.kt
@@ -1,7 +1,28 @@
 package com.flint.presentation.collectioncreate
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.flint.core.designsystem.component.textfield.FlintSearchTextField
+import com.flint.core.designsystem.component.topappbar.FlintBackTopAppbar
+import com.flint.core.designsystem.theme.FlintTheme
+import com.flint.presentation.collectioncreate.component.CollectionCreateFilmSelect
 
 @Composable
 fun AddFilmRoute(
@@ -10,6 +31,112 @@ fun AddFilmRoute(
 ) {
 }
 
+data class CollectionFilmUiModel(
+    val filmId: Long,
+    val imageUrl: String,
+    val title: String,
+    val director: String,
+    val createdYear: String,
+)
+
 @Composable
-fun AddFilmScreen() {
+fun AddFilmScreen(
+    onBackClick: () -> Unit,
+) {
+    var selectedFilmIds by remember { mutableStateOf(setOf<Long>()) }
+    var searchText by remember { mutableStateOf("") }
+
+    val filmList = remember {
+        mutableStateListOf(
+            CollectionFilmUiModel(
+                filmId = 1L,
+                imageUrl = "https://buly.kr/DEaVFRZ",
+                title = "해리포터 불의 잔",
+                director = "마이크 뉴웰",
+                createdYear = "2005",
+            ),
+            CollectionFilmUiModel(
+                filmId = 2L,
+                imageUrl = "https://buly.kr/DEaVFRZ",
+                title = "인터스텔라",
+                director = "크리스토퍼 놀란",
+                createdYear = "2014",
+            ),
+            CollectionFilmUiModel(
+                filmId = 3L,
+                imageUrl = "https://buly.kr/DEaVFRZ",
+                title = "라라랜드",
+                director = "데이미언 셔젤",
+                createdYear = "2016",
+            ),
+        )
+    }
+
+    Column(
+        modifier =Modifier
+            .fillMaxWidth()
+            .background(color = FlintTheme.colors.background),
+    ) {
+        FlintBackTopAppbar(
+            onClick = onBackClick,
+            title = "작품 추가하기",
+            actionText = "추가",
+            textColor = FlintTheme.colors.gray300,
+        )
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+//        modifier = Modifier
+//            .fillMaxWidth()
+//            .padding(horizontal = 16.dp),
+
+        FlintSearchTextField(
+            placeholder = "추천하고 싶은 작품을 검색해보세요",
+            value = searchText,
+            onValueChanged = { searchText = it },
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        LazyColumn (
+            modifier = Modifier.weight(1f),
+            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            // 작품 리스트
+            items(
+                items = filmList,
+                key = { it.filmId },
+            ) { film ->
+                val isSelected = selectedFilmIds.contains(film.filmId)
+
+                CollectionCreateFilmSelect(
+                    onCheckClick = {
+                        selectedFilmIds =
+                            if (isSelected) {
+                                selectedFilmIds - film.filmId
+                            } else {
+                                selectedFilmIds + film.filmId
+                            }
+                    },
+                    isSelected = isSelected,
+                    imageUrl = film.imageUrl,
+                    title = film.title,
+                    director = film.director,
+                    createdYear = film.createdYear,
+                )
+            }
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun AddFilmScreenPreview() {
+    FlintTheme {
+        AddFilmScreen(
+            onBackClick = {}
+        )
+    }
 }
