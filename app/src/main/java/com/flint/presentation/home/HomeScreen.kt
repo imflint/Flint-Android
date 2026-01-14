00a7e8ae31037d90a1f51eb0eedda41fcbbe334b
kimjw <kimjw111003@gmail.com>
2026-01-14 22:52:57 +0900

    feat: home header, banner

--- app/src/main/java/com/flint/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/flint/presentation/home/HomeScreen.kt
@@ -3,13 +3,20 @@ package com.flint.presentation.home
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.flint.core.designsystem.component.topappbar.FlintLogoTopAppbar
 import com.flint.core.designsystem.theme.FlintTheme
+import com.flint.presentation.home.component.HomeBanner
 
 @Composable
 fun HomeRoute(
@@ -18,22 +25,50 @@ fun HomeRoute(
     navigateToCollectionCreate: () -> Unit,
 ) {
     HomeScreen(
+        userName = "종우",
         modifier = Modifier.padding(paddingValues),
     )
 }
 
 @Composable
-private fun HomeScreen(modifier: Modifier = Modifier) {
+private fun HomeScreen(
+    userName: String,
+    modifier: Modifier = Modifier
+) {
     Box(
-        modifier =
-            modifier
-                .background(FlintTheme.colors.background)
-                .fillMaxSize(),
+        modifier = modifier
+            .background(FlintTheme.colors.background)
+            .fillMaxSize(),
         contentAlignment = Alignment.Center,
     ) {
-        Text(
-            text = "HomeScreen",
-            color = FlintTheme.colors.white,
-        )
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            stickyHeader {
+                FlintLogoTopAppbar()
+            }
+
+            item {
+                Spacer(Modifier.height(5.dp))
+
+                HomeBanner(
+                    userName = userName
+                )
+            }
+
+            item {
+                Spacer(Modifier.height(48.dp))
+
+
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewHomeScreen() {
+    FlintTheme {
+        HomeScreen(userName = "종우",)
     }
 }
---
+++ app/src/main/java/com/flint/presentation/home/component/HomeBanner.kt
@@ -0,0 +1,52 @@
+package com.flint.presentation.home.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.flint.R
+import com.flint.core.designsystem.theme.FlintTheme
+
+@Composable
+fun HomeBanner(
+    userName: String,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(270.dp)
+            .paint(
+                painter = painterResource(id = R.drawable.img_collection_bg2),
+                contentScale = ContentScale.FillBounds
+            )
+    ) {
+        Text(
+            text = "반가워요, $userName 님\n오늘은 어떤 작품이 끌리세요?",
+            style = FlintTheme.typography.head1Sb22,
+            color = FlintTheme.colors.white,
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .padding(start = 16.dp, bottom = 30.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewHomeBanner() {
+    FlintTheme {
+        HomeBanner(
+            userName = "종우"
+        )
+    }
+}
