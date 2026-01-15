d4e68b4cc05f50767df97d62dab1789ccf06e7c7
kimjw <kimjw111003@gmail.com>
2026-01-16 00:55:31 +0900

    chore: 코드리뷰 적용

--- app/src/main/java/com/flint/presentation/splash/SplashScreen.kt
+++ app/src/main/java/com/flint/presentation/splash/SplashScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
 @Composable
