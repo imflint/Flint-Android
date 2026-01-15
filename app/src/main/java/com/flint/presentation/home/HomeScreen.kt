8c4a1e7c16b749261aa7ca02839a8d903ce9a00c
Nahyun Kim <hedghog1004@naver.com>
2026-01-15 18:40:27 +0900

    refactor: 홈 프리뷰 더미데이터 수정

--- app/src/main/java/com/flint/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/flint/presentation/home/HomeScreen.kt
@@ -17,11 +17,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.flint.core.designsystem.component.topappbar.FlintLogoTopAppbar
 import com.flint.core.designsystem.theme.FlintTheme
-import com.flint.domain.model.AuthorModel
 import com.flint.domain.model.CollectionModel
 import com.flint.domain.model.ContentModel
-import com.flint.domain.type.OttType
-import com.flint.domain.type.UserRoleType
 import com.flint.presentation.home.component.HomeBanner
 import com.flint.presentation.home.component.HomeFab
 import com.flint.presentation.home.component.HomeRecentCollection
@@ -144,87 +141,8 @@ private fun HomeScreen(
 @Composable
 private fun PreviewHomeScreen() {
     FlintTheme {
-        val collectionModelList =
-            listOf(
-                CollectionModel(
-                    collectionId = "",
-                    collectionTitle = "컬렉션 제목",
-                    collectionImageUrl = "",
-                    createdAt = "",
-                    isBookmarked = false,
-                    author =
-                        AuthorModel(
-                            userId = 0,
-                            nickname = "사용자 이름",
-                            profileUrl = "",
-                            userRole = UserRoleType.FLINER,
-                        ),
-                ),
-                CollectionModel(
-                    collectionId = "",
-                    collectionTitle = "컬렉션 제목2",
-                    collectionImageUrl = "",
-                    createdAt = "",
-                    isBookmarked = false,
-                    author =
-                        AuthorModel(
-                            userId = 0,
-                            nickname = "사용자 이름2",
-                            profileUrl = "",
-                            userRole = UserRoleType.FLINER,
-                        ),
-                ),
-            )
-
-        val contentModelList =
-            listOf(
-                ContentModel(
-                    contentId = 0,
-                    title = "드라마 제목",
-                    year = 2000,
-                    posterImage = "",
-                    ottSimpleList =
-                        listOf(
-                            OttType.Netflix,
-                            OttType.Disney,
-                            OttType.Tving,
-                            OttType.Coupang,
-                        ),
-                ),
-                ContentModel(
-                    contentId = 0,
-                    title = "드라마 제목2",
-                    year = 2020,
-                    posterImage = "",
-                    ottSimpleList =
-                        listOf(
-                            OttType.Wave,
-                            OttType.Watcha,
-                            OttType.Tving,
-                        ),
-                ),
-                ContentModel(
-                    contentId = 0,
-                    title = "드라마 제목3",
-                    year = 2003,
-                    posterImage = "",
-                    ottSimpleList =
-                        listOf(
-                            OttType.Disney,
-                            OttType.Tving,
-                        ),
-                ),
-                ContentModel(
-                    contentId = 0,
-                    title = "드라마 제목4",
-                    year = 1919,
-                    posterImage = "",
-                    ottSimpleList =
-                        listOf(
-                            OttType.Watcha,
-                        ),
-                ),
-            )
+        val collectionModelList = CollectionModel.FakeList
+        val contentModelList = ContentModel.FakeList
 
         HomeScreen(
             userName = "종우",
