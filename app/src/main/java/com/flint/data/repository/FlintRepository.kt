90250bfa913dbeeb5e334571a6b5f469ae75d0be
Nahyun Kim <hedghog1004@naver.com>
2026-01-18 02:52:51 +0900

    delete: Sample 파일 삭제

--- app/src/main/java/com/flint/data/repository/FlintRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.flint.data.repository
-
-import com.flint.data.dto.SampleRequestDto
-import com.flint.domain.model.SampleModel
-
-interface FlintRepository {
-    suspend fun sample(requestDto: SampleRequestDto): Result<SampleModel>
-}
