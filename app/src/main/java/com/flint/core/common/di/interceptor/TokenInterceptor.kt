f3b383a8206c1d127036b2453ddf007d2d2cf4ea
Giovanni Junseo Kim <kingjunseo@gmail.com>
2026-01-20 05:27:34 +0900

    style: TokenInterceptor 뎁스 초기화

--- app/src/main/java/com/flint/core/common/di/interceptor/TokenInterceptor.kt
+++ app/src/main/java/com/flint/core/common/di/interceptor/TokenInterceptor.kt
@@ -9,28 +9,28 @@ import okhttp3.Response
 import javax.inject.Inject
 
 class TokenInterceptor
-@Inject
-constructor(
-    private val preferencesManager: PreferencesManager,
-) : Interceptor {
-    override fun intercept(chain: Interceptor.Chain): Response {
-        val originalRequest = chain.request()
+    @Inject
+    constructor(
+        private val preferencesManager: PreferencesManager,
+    ) : Interceptor {
+        override fun intercept(chain: Interceptor.Chain): Response {
+            val originalRequest = chain.request()
 
-        val accessToken =
-            runBlocking {
-                preferencesManager.getString(ACCESS_TOKEN).first()
+            val accessToken =
+                runBlocking {
+                    preferencesManager.getString(ACCESS_TOKEN).first()
+                }
+
+            val requestBuilder = originalRequest.newBuilder()
+
+            if (accessToken.isNotEmpty()) {
+                requestBuilder.header("Authorization", "Bearer $accessToken")
             }
+            // sample
+            requestBuilder.header("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJ1c2VySWQiOjgwMDAyMTcxMTM1NzExODE5MSwicm9sZSI6IkZMSU5HIiwidHlwZSI6IkFDQ0VTUyIsImlhdCI6MTc2ODgzODU0MCwiZXhwIjoxNzcwMDQ4MTQwfQ.A2XYyu24IoGAXNSQHJ1S-iudWmg8II2_ivI4EdyyWw9KS9oJlxHKOAhcKrsLpLkc9kllZyxwaTJO1t4vI7oZlg")
 
-        val requestBuilder = originalRequest.newBuilder()
+            val request = requestBuilder.build()
 
-        if (accessToken.isNotEmpty()) {
-            requestBuilder.header("Authorization", "Bearer $accessToken")
+            return chain.proceed(request)
         }
-        // sample
-        requestBuilder.header("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJ1c2VySWQiOjgwMDAyMTcxMTM1NzExODE5MSwicm9sZSI6IkZMSU5HIiwidHlwZSI6IkFDQ0VTUyIsImlhdCI6MTc2ODgzODU0MCwiZXhwIjoxNzcwMDQ4MTQwfQ.A2XYyu24IoGAXNSQHJ1S-iudWmg8II2_ivI4EdyyWw9KS9oJlxHKOAhcKrsLpLkc9kllZyxwaTJO1t4vI7oZlg")
-
-        val request = requestBuilder.build()
-
-        return chain.proceed(request)
     }
-}
