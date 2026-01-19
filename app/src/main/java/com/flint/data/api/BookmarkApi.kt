8591ee56be633680c79cda9c71699fe7d1ba69e6
Giovanni Junseo Kim <kingjunseo@gmail.com>
2026-01-20 05:30:28 +0900

    style: BookmarkApi 수정

--- app/src/main/java/com/flint/data/api/BookmarkApi.kt
+++ app/src/main/java/com/flint/data/api/BookmarkApi.kt
@@ -5,8 +5,13 @@ import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface BookmarkApi {
+    // 컬렉션 북마크 유저 조회
+
+    // 컬렉션 북마크 토글
     @POST("api/v1/bookmarks/collections/{collectionId}")
     suspend fun toggleCollectionBookmark(
         @Path("collectionId") collectionId: String,
     ): BaseResponse<Boolean>
+
+    // 콘텐츠 북마크 토글
 }
