37bd9acf5533104234e1f166a27fd47c0df1448e
ckals413 <ckals413@naver.com>
2026-01-22 20:51:31 +0900

    fix: Ott 이름 변경

--- app/src/main/java/com/flint/domain/type/OttType.kt
+++ app/src/main/java/com/flint/domain/type/OttType.kt
@@ -10,8 +10,8 @@ enum class OttType(
 ) {
     Netflix(1L, R.drawable.img_netflix, R.drawable.ic_small_netflix, "넷플릭스"),
     Tving(2L, R.drawable.img_tving, R.drawable.ic_small_tving, "티빙"),
-    Coupang(3L, R.drawable.img_coupang, R.drawable.ic_small_coupang, "쿠팡플레이"),
-    Wave(4L, R.drawable.img_wave, R.drawable.ic_small_wave, "웨이브"),
+    CoupangPlay(3L, R.drawable.img_coupang, R.drawable.ic_small_coupang, "쿠팡플레이"),
+    Wavve(4L, R.drawable.img_wave, R.drawable.ic_small_wave, "웨이브"),
     Disney(5L, R.drawable.img_disney, R.drawable.ic_small_disney, "디즈니플러스"),
     Watcha(6L, R.drawable.img_watcha, R.drawable.ic_small_watcha, "왓챠피디아"),
 }
